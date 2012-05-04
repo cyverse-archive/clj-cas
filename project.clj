3a857cc5454a3da02c3e08157f3a3bfe5db08513
Dennis Roberts <dennis@iplantcollaborative.org>
2012-05-04 11:01:07 -0700

    CORE-3146: added the CAS proxy authentication code

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
-(defproject clj-cas "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject org.iplantc/clj-cas "1.0.0-SNAPSHOT"
+  :description "A CAS Client library written in Clojure."
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.jasig.cas.client/cas-client-core "3.2.0"
+                  :exclusions [javax.servlet/servlet-api]]])
--- test/clj_cas/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns clj-cas.test.core
-  (:use [clj-cas.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
